8979db85f506967ab17e65a73dc1534fc4e85b61
Asad <asadbekasadbek@gmail.com>
2023-08-24 11:08:41 -0400

    chore: initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,70 +2,92 @@
 
 import Head from 'next/head';
 import * as React from 'react';
+import ReactAllPlayer from 'react-all-player';
+import { BsGithub } from 'react-icons/bs';
 
-import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from '~/svg/Logo.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 export default function HomePage() {
   return (
     <main>
       <Head>
-        <title>Hi</title>
+        <title>ReactAllPlayer</title>
       </Head>
-      <section className='bg-white'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-          <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
-          </p>
-          <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
-            </ArrowLink>
-          </p>
 
-          <ButtonLink className='mt-6' href='/components' variant='light'>
-            See all components
-          </ButtonLink>
+      <div className='flex min-h-screen items-center justify-center'>
+        <div className='mx-4 grid h-full grid-cols-1 space-y-12 duration-100  ease-in-out md:mx-24 md:grid-cols-2'>
+          <div className='mx-4 flex w-full flex-col items-center justify-center space-y-6 md:mx-6 md:w-2/3 md:items-start'>
+            <div className='font-bold  tracking-wide text-red-600 [font-size:var(--step-4)]'>
+              ReactAllPlayer
+            </div>
 
-          <UnstyledLink
-            href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-            className='mt-4'
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width='92'
-              height='32'
-              src='https://vercel.com/button'
-              alt='Deploy with Vercel'
+            <div className=''>
+              <span className='leading-relaxed text-neutral-800 [font-size:var(--step-1)]'>
+                Simple React component that provides versatile and good looking
+                UI video player.
+              </span>
+            </div>
+
+            <div>
+              <ButtonLink
+                rightIcon={BsGithub}
+                variant='primary'
+                className='px-6 py-3 [font-size:var(--step--1)]'
+                href='https://github.com/asadbek064/react-all-player'
+              >
+                Download
+              </ButtonLink>
+            </div>
+          </div>
+
+          <div className='flex flex-col justify-center'>
+            <ReactAllPlayer
+              className='max-h-full max-w-full items-center'
+              autoPlay={true}
+              thumbnail='https://cdn.plyr.io/static/demo/thumbs/100p-00001.jpg'
+              sources={[
+                {
+                  file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
+                  label: '1080p',
+                },
+                {
+                  file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
+                  label: '720p',
+                },
+              ]}
+              subtitles={[
+                {
+                  lang: 'en',
+                  language: 'English',
+                  file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt',
+                },
+                {
+                  lang: 'fr',
+                  language: 'French',
+                  file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt',
+                },
+              ]}
             />
-          </UnstyledLink>
 
-          <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
-            </UnderlineLink>
-          </footer>
+            <small className='flex-start mt-1 flex text-[0.55rem] text-neutral-800 opacity-40'>
+              <a
+                href='https://itunes.apple.com/au/movie/view-from-a-blue-moon/id1041586323'
+                target='_blank'
+                className='link'
+              >
+                View From A Blue Moon © Brainfarm
+              </a>
+            </small>
+          </div>
         </div>
-      </section>
+      </div>
+
+      <footer className='absolute bottom-0 w-full bg-neutral-50 py-2 text-center text-gray-900'>
+        © {new Date().getFullYear()} By{' '}
+        <UnderlineLink href='https://asadbek.dev/'>
+          Asadbek Karimov
+        </UnderlineLink>
+      </footer>
     </main>
   );
 }
